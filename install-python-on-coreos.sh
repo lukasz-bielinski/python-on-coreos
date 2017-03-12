103e552ed83d69296beb318f21956f1e9b480ff4
lukasz-bielinski <lukasz-bielinski@users.noreply.github.com>
2017-03-12 13:01:07 +0100

    updated version

--- install-python-on-coreos.sh
+++ install-python-on-coreos.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
-VERSIONS=${VERSIONS:-"2.7.8.10"}
+VERSIONS=${VERSIONS:-"2.7.13.2713"}
 
 # make directory
 mkdir -p /opt/bin
 cd /opt
+wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785.tar.gz
+tar -xzvf ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785.tar.gz
 
-wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/ActivePython-${VERSIONS}-linux-x86_64.tar.gz
-tar -xzvf ActivePython-${VERSIONS}-linux-x86_64.tar.gz
-
-mv ActivePython-${VERSIONS}-linux-x86_64 apy && cd apy && ./install.sh -I /opt/python/
+mv ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785 apy && cd apy && ./install.sh -I /opt/python/
 
 ln -s /opt/python/bin/easy_install /opt/bin/easy_install
 ln -s /opt/python/bin/pip /opt/bin/pip
