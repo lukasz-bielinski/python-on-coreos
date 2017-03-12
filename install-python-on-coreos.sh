a9572302e8b9a9095bf066f791345178b8711f3e
lukasz-bielinski <lukasz-bielinski@users.noreply.github.com>
2017-03-12 13:11:20 +0100

    remove sudo

--- install-python-on-coreos.sh
+++ install-python-on-coreos.sh
@@ -2,14 +2,14 @@
 VERSIONS=${VERSIONS:-"2.7.13.2713"}
 
 # make directory
-sudo mkdir -p /opt/bin
+mkdir -p /opt/bin
 cd /opt
-sudo wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785.tar.gz
-sudo tar -xzvf ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785.tar.gz
+wget http://downloads.activestate.com/ActivePython/releases/${VERSIONS}/ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785.tar.gz
+tar -xzvf ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785.tar.gz
 
-sudo mv ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785 apy && cd apy && sudo ./install.sh -I /opt/python/
+mv ActivePython-${VERSIONS}-linux-x86_64-glibc-2.3.6-401785 apy && cd apy &&  ./install.sh -I /opt/python/
 
-sudo ln -s /opt/python/bin/easy_install /opt/bin/easy_install
-sudo ln -s /opt/python/bin/pip /opt/bin/pip
-sudo ln -s /opt/python/bin/python /opt/bin/python
-sudo ln -s /opt/python/bin/virtualenv /opt/bin/virtualenv
+ln -s /opt/python/bin/easy_install /opt/bin/easy_install
+ln -s /opt/python/bin/pip /opt/bin/pip
+ln -s /opt/python/bin/python /opt/bin/python
+ln -s /opt/python/bin/virtualenv /opt/bin/virtualenv
